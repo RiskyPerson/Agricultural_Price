c58cc3f88052bb8fa5defc804d6d44f60668c63f
HuyTNG <anhdevwen@gmail.com>
2023-05-17 00:14:07 +0700

    Test random price

--- src/main/java/com/example/agriculturals/price/service/AgriculturalPriceService.java
+++ src/main/java/com/example/agriculturals/price/service/AgriculturalPriceService.java
@@ -106,6 +106,7 @@ public class AgriculturalPriceService {
 //            String rand = randomPrice(agriculturalPriceDTOList.get(i).getPrice(), agriculturalPriceDTOList.get(i).getType());
             result.add(agriculturalPriceList.get(i).getPrice());
             result.add(agriculturalPriceList.get(i).getAgricultural().getType());
+            result.add(randomPrice(agriculturalPriceList.get(i).getPrice(), agriculturalPriceList.get(i).getAgricultural().getType()));
         }
 
         result.add("Random text");
